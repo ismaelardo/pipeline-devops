8931ad4ae07c73445f97b28538d29154163c28ee
ismaelardo <ismaelardo@gmail.com>
2022-01-31 23:46:30 -0300

    diferenciación tipo de pipeline CI o CD

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -5,63 +5,110 @@
 */
 
 def call(stages, String pipelineType){
+    figlet 'Gradle'
+    if (pipelineType == 'CI'){
 
-    figlet pipelineType
+        figlet 'Integración Contínua'
 
-	stage('Build & Unit test') {
-		STAGE = env.STAGE_NAME
-        if (stages == STAGE) {
-            sh './gradlew clean build'
+        stage('Build & Unit test') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh './gradlew clean build'
+            }       
         }
-                    //println "Stage: ${env.STAGE_NAME}"
-    }
 
-    stage('SonarQube analysis'){
-    	STAGE = env.STAGE_NAME
-        if (stages == STAGE) {
-            def scannerHome = tool 'sonar-scanner';
-            withSonarQubeEnv('sonarqube-server'){
-                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle-nuevo -Dsonar.sources=src -Dsonar.java.binaries=build"
+        stage('SonarQube analysis'){
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"
+                def scannerHome = tool 'sonar-scanner';
+                withSonarQubeEnv('sonarqube-server'){
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle-nuevo -Dsonar.sources=src -Dsonar.java.binaries=build"
+                }
+            }
+        }
+
+        stage('Run') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh "nohup bash ./gradlew bootRun &"
+                sleep 20
+            }
+        }
+
+        stage('Test') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+            }
+        }
+        
+        stage('nexus') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"
+                nexusPublisher nexusInstanceId: 'nexus_test',
+                nexusRepositoryId: 'test_nexus',
+                packages: [
+                    [
+                        $class: 'MavenPackage',
+                        mavenAssetList: [
+                            [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/libs/DevOpsUsach2020-0.0.1.jar"]
+                        ],
+                        mavenCoordinate: [
+                            artifactId: 'DevOpsUsach2020',
+                            groupId: 'com.devopsusach2020',
+                            packaging: 'jar',
+                            version: '0.0.1'
+                        ]
+                    ]
+                ]
+            }
+        }
+
+
+    } else {
+
+        figlet 'Delivery Contínuo'
+        stage('DownloadSnapshotJar') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"                
+            }
+        }
+
+        stage('RunSnapshotJar') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"                
+            }
+        }
+
+        stage('TestSnapshotJar') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"                
+            }
+        }
+
+        stage('UploadReleaseJar') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"                
             }
         }
     }
 
-    stage('Run') {
-    	STAGE = env.STAGE_NAME
-        if (stages == STAGE) {
-            sh "nohup bash ./gradlew bootRun &"
-            sleep 20
-        }
-    }
+	
 
-    stage('Test') {
-    	STAGE = env.STAGE_NAME
-        if (stages == STAGE) {
-            sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-        }
-    }
     
-    stage('nexus') {
-    	STAGE = env.STAGE_NAME
-        if (stages == STAGE) {
-            nexusPublisher nexusInstanceId: 'nexus_test',
-            nexusRepositoryId: 'test_nexus',
-            packages: [
-                [
-                    $class: 'MavenPackage',
-                    mavenAssetList: [
-                        [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/libs/DevOpsUsach2020-0.0.1.jar"]
-                    ],
-                    mavenCoordinate: [
-                        artifactId: 'DevOpsUsach2020',
-                        groupId: 'com.devopsusach2020',
-                        packaging: 'jar',
-                        version: '0.0.1'
-                    ]
-                ]
-            ]
-        }
-    }
+
+    
+
+    
 }
 
 return this;
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -6,39 +6,58 @@
 
 def call(stages, String pipelineType){
 
-    figlet pipelineType
-  
-  	stage('Compile') {
-        STAGE = env.STAGE_NAME
-        if (stages == STAGE) {
-            sh './mvnw clean compile -e'
+    figlet 'Maven'
+
+    if (pipelineType == 'CI'){
+
+        figlet 'Integración Contínua'
+
+        stage('Compile') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE) {
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh './mvnw clean compile -e'
+            }
         }
-    }
-    stage('Test') {
-    	STAGE = env.STAGE_NAME
-         if (stages == STAGE){
-            sh './mvnw clean test -e'    
+        stage('Test') {
+            STAGE = env.STAGE_NAME
+             if (stages == STAGE){
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh './mvnw clean test -e'    
+            }
         }
-    }
-    
-    stage('Jar') {
-    	STAGE = env.STAGE_NAME
-        if (stages == STAGE){
-            sh './mvnw clean package -e'
+        
+        stage('Jar') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE){
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh './mvnw clean package -e'
+            }
         }
-    }
-    stage('Run') {
-    	STAGE = env.STAGE_NAME
-        if (stages == STAGE){
-            sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &'
+            
+    } else {
+
+        figlet 'Delivery Contínuo'
+
+        stage('Run') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE){
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &'
+            }
         }
-    }
-    stage('Testing') {
-    	STAGE = env.STAGE_NAME
-        if (stages == STAGE){
-            sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        stage('Testing') {
+            STAGE = env.STAGE_NAME
+            if (stages == STAGE){
+                figlet "Stage: ${env.STAGE_NAME}"
+                sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+            }
         }
+
     }
+
+  	
+
     
 }
 
